e40621eb9b1336bd281c757dba031f2edf85f10b
Nalini Karina Andhini <nalinikarina613@gmail.com>
2026-04-30 12:08:45 +0700

    instalasi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-6xl text-amber-300 p-7'>
+hahhhh
+    </div>
+  )
+}
+
+export default App
